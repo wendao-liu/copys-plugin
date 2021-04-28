b2b4112dc566e70cbb727ab15854f901fcb10a63
wb-lhy498637 <wb-lhy498637@alibaba-inc.com>
2021-04-28 11:13:18 +0800

    feat: 稳定版本v1.0,兼容mac window

--- main/copys-plugin.js
+++ main/copys-plugin.js
@@ -1,5 +1,8 @@
 const path = require('path');
+var rimraf = require("rimraf");
 const fs = require('fs');
+const pinPath = process.platform === 'darwin' ? '/' : '\\';
+const projectRoot = process.cwd();
 module.exports = class CpPlugin {
     constructor(options) {
         this.options = options;
@@ -8,12 +11,14 @@ module.exports = class CpPlugin {
         compiler.hooks.done.tap(
             'CpPlugin',
             (status) => {
-                const { from, to, pub } = this.options || {};
-                const projectRoot = process.cwd();
+                let { from, to } = this.options || {};
+                from = path.join(projectRoot, from).replace(/\//g, pinPath);
+                deleteDir(to);
+                to = path.join(projectRoot, to).replace(/\//g, pinPath);
+                console.log(from, '---from', to, '---to');
                 copyDir(
-                    path.join(projectRoot, from),
-                    path.join(projectRoot, to),
-                    pub,
+                    from,
+                    to,
                     function (err) {
                         if (err) {
                             console.log('编译失败！');
@@ -27,21 +32,39 @@ module.exports = class CpPlugin {
     }
 }
 
-
+function deleteDir(p) {
+    let deleteTo = '';
+    let log = 0;
+    for (let index = 0; index < p.length; index++) {
+        if (p[index] === '/') {
+            log++;
+        }
+        if (log === 3) {
+            break;
+        } else {
+            deleteTo += p[index];
+        }
+    }
+    deleteTo = path.join(projectRoot, deleteTo).replace(/\//g, pinPath);
+    try {
+        rimraf.sync(deleteTo);
+    } catch (e) {
+        console.log('不存在文件');
+    }
+}
 
 /*
  * 复制目录、子目录，及其中的文件
  * @param src {String} 要复制的目录
  * @param dist {String} 复制到目标目录
  */
-function copyDir(src, dist, pub, callback) {
+function copyDir(src, dist, callback) {
     fs.access(dist, function (err) {
         if (err) {
             // 目录不存在时创建目录
-            fs.mkdirSync(dist);
-            fs.mkdirSync(dist + '/' + pub);
+            fs.mkdirSync(dist, { recursive: true });
         }
-        _copy(null, src, dist + '/' + pub);
+        _copy(null, src, dist + pinPath);
     });
 
     function _copy(err, src, dist) {
@@ -53,8 +76,8 @@ function copyDir(src, dist, pub, callback) {
                     callback(err)
                 } else {
                     paths.forEach(function (path) {
-                        var _src = src + '/' + path;
-                        var _dist = dist + '/' + path;
+                        var _src = src + pinPath + path;
+                        var _dist = dist + pinPath + path;
                         fs.stat(_src, function (err, stat) {
                             if (err) {
                                 callback(err);
